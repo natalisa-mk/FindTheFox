53b83cc17f5926f1910b049b7bfba660a77f7c38
kicadrica <92972020+kicadrica@users.noreply.github.com>
2024-11-19 13:24:04 +0100

    Deleted text in WinPanel;

--- Assets/Scripts/StartMenuAnimation.cs
+++ Assets/Scripts/StartMenuAnimation.cs
@@ -12,6 +12,7 @@ public class StartMenuAnimation : MonoBehaviour
     private const float AnimDelay = 1f;
     private const float HeaderMoveDuration = 1.5f;
     private const float MenuFadeDuration = 2f;
+    private const float IntervalBetweenAnim = 0.5f;
     
     private void Start()
     {
@@ -26,8 +27,8 @@ public class StartMenuAnimation : MonoBehaviour
         var sequence = DOTween.Sequence();
 
         sequence.AppendInterval(AnimDelay);
-        sequence.Append(header.DOAnchorPosY(headerFinalPosY, HeaderMoveDuration));
-        sequence.AppendInterval(0.5f);
+        sequence.Append(header.DOAnchorPosY(headerFinalPosY, HeaderMoveDuration).SetEase(Ease.OutCubic));
+        sequence.AppendInterval(IntervalBetweenAnim);
         sequence.Append(_menuCanvasGroup.DOFade(1, MenuFadeDuration));
     }
 }
